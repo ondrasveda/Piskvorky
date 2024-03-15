17646af675074e16f470124700435cbe9f599163
landa2 <landamartin220@gmail.com>
2024-03-15 13:18:17 +0100

    Added checkTie method and switchPLayer method

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -18,5 +18,26 @@ public class TicTacToe {
         }
     }
 
+    private static void checkTie() {
+        boolean fullBoard = true;
+        for (int i = 0; i < 3; i++){
+            for (int j = 0; j < 3; j++){
+                if (board[i][j] == ' ') {
+                    fullBoard = false;
+                    break;
+                }
+            }
+            if (!fullBoard){
+                break;
+            }
+        }
+        if (fullBoard){
+            System.out.println("It is a tie!");
+            gameOver = true;
+        }
+    }
 
+    private static void switchPlayer(){
+        currentPlayer = (currentPlayer == 'x') ? 'O' : 'X';
+    }
 }
